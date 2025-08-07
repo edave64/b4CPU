1c232346b73c4da1f790e8cf2e7948aa23541bc4
edave64 <github5815@xcvbnm.org>
2025-08-08 00:07:57 +0200

    refactor(pages): Renamed index page to CPU

--- src/pages/CPU.vue
+++ src/pages/CPU.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <cpu :excerciseState="demoExcercise" :decoderState="decoderState" />
+    <cpu :excerciseState="demoExcercise" />
   </q-page>
 </template>
 
@@ -10,10 +10,7 @@ import type { ComputedRef } from 'vue';
 import { computed } from 'vue';
 import cpu from 'components/CPU.vue';
 import excercise from '../config/demoExcercise.json';
-import initialDecoderState from '../config/initialDecoder.json';
 import type { IExcerciseState } from '../interfaces/excercises';
-import type { IDecoderState } from '../interfaces/decoder';
-import { readDecoder, verifyDecoder } from '../engine/readDecoder';
 
 const demoExcercise: ComputedRef<IExcerciseState> = computed(() => {
   return {
@@ -21,8 +18,4 @@ const demoExcercise: ComputedRef<IExcerciseState> = computed(() => {
     instructionMemoryState: excercise.instructionMemoryState,
   };
 });
-if (!verifyDecoder(initialDecoderState)) {
-  throw new Error('Invalid decoder state');
-}
-const decoderState: IDecoderState = readDecoder(initialDecoderState);
 </script>
