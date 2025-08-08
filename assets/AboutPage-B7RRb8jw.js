f632a8de8c25df8bb3ad160f8231d63fd12dfe16
edave64 <github5815@xcvbnm.org>
2025-08-09 00:06:20 +0200

    Update pages

---
+++ assets/AboutPage-B7RRb8jw.js
@@ -0,0 +1,1 @@
+import{Q as t}from"./QPage-CGGyLCLg.js";import{d as o,Q as s,R as r,a1 as n,V as a,U as l}from"./index-rMVkXsJe.js";import"./render-dTF-AMUP.js";const d=o({__name:"AboutPage",setup(p){return(u,e)=>(l(),s(n(t),{padding:""},{default:r(()=>e[0]||(e[0]=[a("div",null,[a("h2",null,"About"),a("p",null,"B4CPU is a simple CPU simulator.")],-1)])),_:1,__:[0]}))}});export{d as default};
