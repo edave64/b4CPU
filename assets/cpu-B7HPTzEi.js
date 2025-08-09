e3946b3b841647220ee6f3ae101d9a82be916c67
edave64 <github5815@xcvbnm.org>
2025-08-09 17:12:05 +0200

    Update pages

---
+++ assets/cpu-B7HPTzEi.js
@@ -0,0 +1,1 @@
+import{a5 as e}from"./index-DEyQJPK9.js";const o=e("cpu",{state:()=>({cpu:null})});export{o as u};
--- assets/cpu-o6b-GK5S.js
+++
@@ -1,1 +0,0 @@
-import{a5 as e}from"./index-rMVkXsJe.js";const o=e("cpu",{state:()=>({cpu:null})});export{o as u};
