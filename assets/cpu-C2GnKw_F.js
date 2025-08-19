d5fa2aeeb118edf28642ab07875ee5c23524afcc
edave64 <github5815@xcvbnm.org>
2025-08-19 22:41:06 +0200

    Update pages

--- assets/cpu-C2GnKw_F.js
+++
@@ -1,1 +0,0 @@
-import{Z as e}from"./index-yeBOKJD4.js";import{m as o}from"./readDecoder-CmVU6KVh.js";const s=e("cpu",{state:()=>({cpu:o()}),actions:{update(t){this.cpu=t}}});export{s as u};
---
+++ assets/cpu-Sb9A57q0.js
@@ -0,0 +1,1 @@
+import{Z as e}from"./index-DUKBsuaK.js";import{m as o}from"./readDecoder-CmVU6KVh.js";const s=e("cpu",{state:()=>({cpu:o()}),actions:{update(t){this.cpu=t}}});export{s as u};
