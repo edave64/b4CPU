34342a7d60dc89c843ab04489744a12ba10281fe
edave64 <github5815@xcvbnm.org>
2025-08-24 01:57:52 +0200

    Prepare excercise system

--- src/pages/CPU.vue
+++ src/pages/CPU.vue
@@ -1,20 +1,13 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <Cpu
-      :excerciseState="demoExcercise"
-      v-model:cpu="cpu"
-      :decoder-state="decoderState"
-    />
+    <Cpu v-model:cpu="cpu" :decoder-state="decoderState" />
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { QPage } from 'quasar';
-import type { ComputedRef } from 'vue';
 import { computed } from 'vue';
 import Cpu from 'components/CPU.vue';
-import excercise from '../config/demoExcercise.json';
-import type { IExcerciseState } from '../interfaces/excercises';
 import { useCpuStore } from '../stores/cpu';
 import type { CpuState } from '../engine/cpu';
 import { useDecoderStore } from '../stores/decoder';
@@ -29,12 +22,5 @@ const cpu = computed({
   set(newCpu: CpuState) {
     cpuStore.update(newCpu);
   },
-});
-
-const demoExcercise: ComputedRef<IExcerciseState> = computed(() => {
-  return {
-    dataMemoryState: excercise.dataMemoryState,
-    instructionMemoryState: excercise.instructionMemoryState,
-  };
 });
 </script>
---
+++ src/pages/EditExcercise.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <Cpu
+      :decoder-state="decoderState"
+      allow-mask-editing
+      v-model:mask="mask"
+      v-model:cpu="initialState"
+    />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { QPage } from 'quasar';
+import { computed, ref } from 'vue';
+import Cpu from 'components/CPU.vue';
+import { makeCpuState, type CpuState } from '../engine/cpu';
+import { useDecoderStore } from '../stores/decoder';
+
+const decoderState = computed(() => useDecoderStore().state);
+
+const initialState = ref<CpuState>(makeCpuState());
+
+const mask = ref<Uint8Array>(makeCpuState());
+</script>
