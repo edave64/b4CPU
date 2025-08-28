7a1f46efba198dd0c4dff3c2632bad70c75cd5a3
edave64 <github5815@xcvbnm.org>
2025-08-28 23:34:46 +0200

    docs: extracted start page from tutorial

--- src/pages/TutorialPage.vue
+++ src/pages/TutorialPage.vue
@@ -59,35 +59,7 @@
     </q-toolbar>
     <q-tab-panels v-model="tutorial.chapter">
       <q-tab-panel name="start">
-        <h4>Welcome to the b4CPU tutorial.</h4>
-        <p>
-          B4CPU is a simulator for a simple 4-bit CPU. It is build to allow you
-          to understand how a CPU works, without getting into overwhelming
-          details.
-        </p>
-        <p>
-          Before you start, make sure you have a basic understanding of
-          <a href="https://en.wikipedia.org/wiki/Binary_number"
-            >binary numbers</a
-          >. It's important to be able to follow the data flowing through the
-          CPU. But don't worry, you only need to count up to 15.
-        </p>
-        <p
-          style="border: 1px solid var(--q-warning)"
-          class="rounded-borders q-pa-xs"
-        >
-          <strong>Warning!</strong> This simulation made to be very simple and
-          not to be accurate to real CPU architectures. E.g. it does not follow
-          the von Neumann architecture that most modern CPUs use and instead
-          seperates instruction memory and data memory.
-        </p>
-        <q-btn
-          stretch
-          flat
-          color="primary"
-          label="Proceed to tour"
-          @click="nextPage"
-        />
+        <StartPage @advance="nextPage" />
       </q-tab-panel>
       <q-tab-panel name="cpu_basics">
         <TourPage @advance="nextPage" />
@@ -104,6 +76,7 @@ import { QPage } from 'quasar';
 import TourPage from './tutorialPages/TourPage.vue';
 import type { Chapter } from '../stores/tutorial';
 import { AllChapters, useTutorial } from '../stores/tutorial';
+import StartPage from './tutorialPages/StartPage.vue';
 
 const tutorial = useTutorial();
 
