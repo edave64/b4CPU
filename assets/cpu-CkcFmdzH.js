87340df6fe1fffaa5cf56b77bfd5bf065e7ee25f
edave64 <github5815@xcvbnm.org>
2025-08-31 23:51:02 +0200

    Update pages

---
+++ assets/cpu-CkcFmdzH.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-AHPH8oyR.js";import{m as o}from"./readDecoder-Cgv1tHFC.js";const s=e("cpu",{state:()=>({cpu:o()}),actions:{update(t){this.cpu=t}}});export{s as u};
---
+++ assets/decoder-DZPBfNVV.js
@@ -0,0 +1,1 @@
+import{W as e,V as t}from"./index-AHPH8oyR.js";import{r,i as o}from"./readDecoder-Cgv1tHFC.js";const a=r(o),c=e("decoder",{state:()=>({state:t(a)})});export{c as u};
